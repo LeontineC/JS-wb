9d949e5ef1a0e5c79060c7ccf8e753abf38a434b
LeontineC <79845918+LeontineC@users.noreply.github.com>
2022-03-29 14:19:57 +0200

    prototypes gallery

---
+++ Gallery/gallery-prototypes.js
@@ -0,0 +1,69 @@
+function Gallery(gallery) {
+  if (!gallery) {
+    throw new Error("no gallery found :( !");
+  }
+  this.gallery = gallery;
+  this.images = Array.from(gallery.querySelectorAll("img"));
+  this.modal = document.querySelector(".modal");
+  this.prevButton = this.modal.querySelector(".prev");
+  this.nextButton = this.modal.querySelector(".next");
+
+  //binding this
+  this.showNextImage = this.showNextImage.bind(this); //at eventListener this points to button not image
+  this.showPrevImage = this.showPrevImage.bind(this);
+  this.handleClickOutside = this.handleClickOutside.bind(this);
+ 
+  this.images.forEach((image) =>
+    image.addEventListener("click", (e) => this.showImage(e.currentTarget))
+  );
+  this.modal.addEventListener("click", this.handleClickOutside);
+}
+
+Gallery.prototype.openModal = function () {
+  console.info("opening modal");
+  if (this.modal.matches(".open")) {
+    console.info("modal already open");
+    return;
+  }
+  this.modal.classList.add("open");
+  this.nextButton.addEventListener("click", this.showNextImage);
+  this.prevButton.addEventListener("click", this.showPrevImage);
+};
+
+Gallery.prototype.closeModal = function () {
+  this.modal.classList.remove("open");
+  this.nextButton.removeEventListener("click", this.showNextImage);
+  this.prevButton.removeEventListener("click", this.showPrevImage);
+};
+
+Gallery.prototype.handleClickOutside = function (e) {
+  if (e.target === e.currentTarget) {
+    this.closeModal();
+  } //currentTarget clicked on outside modal div so if same e.target also outside modal
+};
+
+Gallery.prototype.showNextImage = function () {
+  this.showImage(this.currentImage.nextElementSibling || this.gallery.firstElementChild);
+};
+
+Gallery.prototype.showPrevImage = function () {
+  this.showImage(
+    this.currentImage.previousElementSibling || this.gallery.lastElementChild
+  );
+};
+Gallery.prototype.showImage = function (el) {
+  if (!el) {
+    console.info("no image to show");
+    return;
+  }
+  console.log(el);
+  this.modal.querySelector("img").src = el.src;
+  this.modal.querySelector("h2").textContent = el.title;
+  this.modal.querySelector("h4").textContent = el.dataset.description;
+  this.currentImage = el;
+  this.openModal();
+};
+const gallery1 = new Gallery(document.querySelector(".gallery1"));
+const gallery2 = new Gallery(document.querySelector(".gallery2"));
+//new creates instanceof
+console.log(gallery1, gallery2);
